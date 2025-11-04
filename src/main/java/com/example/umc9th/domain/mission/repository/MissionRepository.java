e42757b1a8616d6bf9e2729359d921f99c6ea989
Inyoung <piypiy9102bbb@gmail.com>
2025-11-04 12:45:36 +0900

    feat: 홈화면 지역에 따른 미션 조회 기능

--- src/main/java/com/example/umc9th/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/example/umc9th/domain/mission/repository/MissionRepository.java
@@ -1,9 +1,24 @@
 package com.example.umc9th.domain.mission.repository;
 
 import com.example.umc9th.domain.mission.entity.Mission;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
 
     // 기본 CRUD 메서드는 JpaRepository에 정의돼있음.
+    @Query("SELECT m FROM Mission m JOIN m.store s " +
+            "WHERE s.address = :selectedAddress " +
+            "AND m.status = 'ONGOING' " +
+            "AND m.dueDate >= :today")
+    Page<Mission> findAvailableMissionsByRegion(
+            @Param("selectedAddress") String selectedAddress,
+            @Param("today") LocalDate today,
+            Pageable pageable
+    );
 }
