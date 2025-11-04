9daed0e3665f5de17ad08aaa9889ce7fdc527c5d
Inyoung <piypiy9102bbb@gmail.com>
2025-11-04 21:45:26 +0900

    fix: MissionRepository 쿼리 미션 status CHALLENGABLE로 변경

--- src/main/java/com/example/umc9th/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/example/umc9th/domain/mission/repository/MissionRepository.java
@@ -8,17 +8,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
 
     // 기본 CRUD 메서드는 JpaRepository에 정의돼있음.
     @Query("SELECT m FROM Mission m JOIN m.store s " +
             "WHERE s.address = :selectedAddress " +
-            "AND m.status = 'ONGOING' " +
+            "AND m.status IN :statusList " +
             "AND m.dueDate >= :today")
     Page<Mission> findAvailableMissionsByRegion(
             @Param("selectedAddress") String selectedAddress,
             @Param("today") LocalDate today,
+            @Param("statusList") List<String> statusList,
             Pageable pageable
     );
 }
