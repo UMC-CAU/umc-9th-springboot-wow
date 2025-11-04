3a42dedd5ff7336da1c6add9575a52e8fed9e6ac
Inyoung <piypiy9102bbb@gmail.com>
2025-11-04 12:12:12 +0900

    feat: MemberMissionRepository @Query + IN 조건

--- src/main/java/com/example/umc9th/domain/member/repository/MemberMissionRepository.java
+++ src/main/java/com/example/umc9th/domain/member/repository/MemberMissionRepository.java
@@ -5,8 +5,17 @@ import com.example.umc9th.domain.mission.enums.Status;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+import java.util.List;
 
 public interface MemberMissionRepository extends JpaRepository<MemberMission, Long> {
-    Page<MemberMission> findAllByMemberIdAndStatus(Long memberId, Status status, Pageable pageable);
-
+    @Query("SELECT mm FROM MemberMission mm JOIN mm.mission m WHERE mm.member.id = :memberId AND m.status IN :statusList")
+    Page<MemberMission> findAllByMemberIdAndStatusIn(
+            @Param("memberId") Long memberId,
+            @Param("statusList") List<Status> statusList,
+            Pageable pageable
+    );
 }
