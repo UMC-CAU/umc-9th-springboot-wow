c565542429e38837e20d9d375f1750454785a6dd
Inyoung <piypiy9102bbb@gmail.com>
2025-11-12 17:03:30 +0900

    fix: 주석 수정

--- src/main/java/com/example/umc9th/global/apiPayload/code/BaseSuccessCode.java
+++ src/main/java/com/example/umc9th/global/apiPayload/code/BaseSuccessCode.java
@@ -2,7 +2,6 @@ package com.example.umc9th.global.apiPayload.code;
 
 import org.springframework.http.HttpStatus;
 
-// BaseErrorCode와 동일한 역할을 수행하지만 성공 케이스에 사용됩니다.
 public interface BaseSuccessCode {
 
     HttpStatus getStatus();
