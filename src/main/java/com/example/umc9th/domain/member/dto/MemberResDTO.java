cada64b6e250607cac25bcb9b540fcda3be08732
Inyoung <piypiy9102bbb@gmail.com>
2025-11-17 20:32:51 +0900

    Feat: MemberResDTO 정의

--- src/main/java/com/example/umc9th/domain/member/dto/MemberResDTO.java
+++ src/main/java/com/example/umc9th/domain/member/dto/MemberResDTO.java
@@ -1,4 +1,13 @@
 package com.example.umc9th.domain.member.dto;
 
+import lombok.Builder;
+import java.time.LocalDateTime;
+
 public class MemberResDTO {
+
+    @Builder
+    public record JoinDTO(
+            Long memberId,
+            LocalDateTime createAt
+    ){}
 }
