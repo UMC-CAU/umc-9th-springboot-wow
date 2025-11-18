6e4138b453170f8cb40fa70f907b94daa99defa9
Inyoung <piypiy9102bbb@gmail.com>
2025-11-18 11:00:57 +0900

    Fix: locationId 타입 Long->Location으로 변경

--- src/main/java/com/example/umc9th/domain/member/dto/MemberReqDTO.java
+++ src/main/java/com/example/umc9th/domain/member/dto/MemberReqDTO.java
@@ -1,5 +1,6 @@
 package com.example.umc9th.domain.member.dto;
 
+import com.example.umc9th.domain.location.entity.Location;
 import com.example.umc9th.domain.member.enums.Gender;
 
 import java.time.LocalDate;
@@ -11,7 +12,7 @@ public class MemberReqDTO {
             String name,
             Gender gender,
             LocalDate birth,
-            Long locationId,
+            Location locationId,
             String addressDetail,
             String email,
             String phoneNumber,
