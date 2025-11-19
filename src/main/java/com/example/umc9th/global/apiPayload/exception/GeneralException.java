80e2954f744b7e351ca53c80520a0c744c1c4461
Inyoung <piypiy9102bbb@gmail.com>
2025-11-19 12:58:09 +0900

    refactor: BaseErrorCode 참조하도록 수정

--- src/main/java/com/example/umc9th/global/apiPayload/exception/GeneralException.java
+++ src/main/java/com/example/umc9th/global/apiPayload/exception/GeneralException.java
@@ -1,17 +1,14 @@
 package com.example.umc9th.global.apiPayload.exception;
 
+import com.example.umc9th.global.apiPayload.code.BaseErrorCode;
 import lombok.Getter;
 
 @Getter
 public class GeneralException extends RuntimeException {
 
-    private final ExceptionCode code;
+    private final BaseErrorCode code;
 
-    /**
-     * 워크북과 달리, DTO의 BaseErrorCode 대신 ExceptionCode를 직접 받도록 변경함.
-     * (ExceptionCode로 BaseErrorCode를 구현해서 이렇게 변경해도 괜찮은가요??)
-     */
-    public GeneralException(ExceptionCode code) {
+    public GeneralException(BaseErrorCode code) {
         super(code.getMessage());
 
         this.code = code;
