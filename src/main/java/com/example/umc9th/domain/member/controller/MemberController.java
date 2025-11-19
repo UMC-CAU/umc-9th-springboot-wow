169e7708c214ea55273a96cdeda8f83e91898b99
Inyoung <piypiy9102bbb@gmail.com>
2025-11-19 12:58:46 +0900

    feat: 회원가입 서비스, 컨트롤러 실습

--- src/main/java/com/example/umc9th/domain/member/controller/MemberController.java
+++ src/main/java/com/example/umc9th/domain/member/controller/MemberController.java
@@ -2,6 +2,8 @@ package com.example.umc9th.domain.member.controller;
 
 import com.example.umc9th.domain.member.dto.MemberReqDTO;
 import com.example.umc9th.domain.member.dto.MemberResDTO;
+import com.example.umc9th.domain.member.exception.code.MemberSuccessCode;
+import com.example.umc9th.domain.member.service.command.MemberCommandService;
 import com.example.umc9th.global.apiPayload.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MemberController {
 
+    private final MemberCommandService memberCommandService;
+
     // 회원가입
     @PostMapping("/sign-up")
     public ApiResponse<MemberResDTO.JoinDTO> signUp(
             @RequestBody MemberReqDTO.JoinDTO dto
     ){
-        return null;
+        return ApiResponse.of(MemberSuccessCode.FOUND, memberCommandService.signup(dto));
     }
 }
