11479f21edad983a37ee8d0feec6465f52a46e14
Inyoung <piypiy9102bbb@gmail.com>
2025-11-24 15:48:30 +0900

    feat: getValue 메서드 추가

--- src/main/java/com/example/umc9th/domain/member/enums/Gender.java
+++ src/main/java/com/example/umc9th/domain/member/enums/Gender.java
@@ -1,5 +1,23 @@
 package com.example.umc9th.domain.member.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Gender {
-    MALE, FEMALE, NONE
+    MALE(1, "남성"),
+    FEMALE(2, "여성"),
+    NONE(3, "선택안함");
+
+    private final Integer value;
+    private final String description;
+
+    Gender(Integer value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    //MemberConverter에서 호출
+    public Integer getValue() {
+        return this.value;
+    }
 }
