489453b1aa5e9ae4474a3665dadcfac111a1ede1
Inyoung <piypiy9102bbb@gmail.com>
2025-11-24 15:52:24 +0900

    refactor: MemberException 생성자 타입을 MemberErrorCode로 변경

--- src/main/java/com/example/umc9th/domain/member/exception/MemberException.java
+++ src/main/java/com/example/umc9th/domain/member/exception/MemberException.java
@@ -1,10 +1,10 @@
 package com.example.umc9th.domain.member.exception;
 
-import com.example.umc9th.global.apiPayload.exception.ExceptionCode;
+import com.example.umc9th.domain.member.exception.code.MemberErrorCode;
 import com.example.umc9th.global.apiPayload.exception.GeneralException;
 
 public class MemberException extends GeneralException {
-    public MemberException(ExceptionCode code) {
+    public MemberException(MemberErrorCode code) {
         super(code);
     }
 }
