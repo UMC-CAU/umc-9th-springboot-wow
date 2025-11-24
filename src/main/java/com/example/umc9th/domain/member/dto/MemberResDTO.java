8c0ba0a92a86efcaae7e846beb38f59441ef5f17
Inyoung <piypiy9102bbb@gmail.com>
2025-11-24 16:34:21 +0900

    fix: 응답 항목 통일 및 수정

--- src/main/java/com/example/umc9th/domain/member/dto/MemberResDTO.java
+++ src/main/java/com/example/umc9th/domain/member/dto/MemberResDTO.java
@@ -1,5 +1,6 @@
 package com.example.umc9th.domain.member.dto;
 
+import com.example.umc9th.domain.member.enums.Gender;
 import lombok.Builder;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,23 +13,17 @@ public class MemberResDTO {
             LocalDateTime createAt
     ){}
 
-    //사용자 상세정보 응답 DTO
     public record MemberDetailDTO(
-            // 기본 정보
             Long memberId,
             String name,
-            String nickname,
             String email,
-            Integer gender, // 또는 String gender
-            Integer age,
+            Gender gender,
             String address,
 
-            // 선호 음식 목록 (이름만 반환)
             List<String> preferFoodNames,
 
-            // 기타 정보
-            LocalDateTime createdAt
-            // LocalDateTime updatedAt
+            LocalDateTime createdAt,
+            LocalDateTime updatedAt
     ) {}
 
     public record PreferFoodListDTO(
