9db0cc2aef950c667d3f9e0c171ac8e6b85abc8c
Inyoung <piypiy9102bbb@gmail.com>
2025-11-24 15:54:09 +0900

    feat: 회원가입 쿼리 서비스 구현

--- src/main/java/com/example/umc9th/domain/member/service/query/MemberQueryServiceImpl.java
+++ src/main/java/com/example/umc9th/domain/member/service/query/MemberQueryServiceImpl.java
@@ -1,4 +1,42 @@
 package com.example.umc9th.domain.member.service.query;
 
-public class MemberQueryServiceImpl {
+import com.example.umc9th.domain.member.converter.MemberConverter;
+import com.example.umc9th.domain.member.dto.MemberResDTO;
+import com.example.umc9th.domain.member.entity.Member;
+import com.example.umc9th.domain.member.entity.mapping.MemberFood;
+import com.example.umc9th.domain.member.exception.MemberException;
+import com.example.umc9th.domain.member.exception.code.MemberErrorCode;
+import com.example.umc9th.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberQueryServiceImpl implements MemberQueryService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public MemberResDTO.MemberDetailDTO getMemberDetail(Long memberId) {
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND));
+
+        List<String> preferFoodNames = member.getPreferredFoods().stream()
+                .map(MemberFood::getFood)
+                .map(food -> food.getName().name())
+                .collect(Collectors.toList());
+
+        return MemberConverter.toDetailDTO(member, preferFoodNames);
+    }
+
+    @Override
+    public boolean isEmailExist(String email) {
+        return memberRepository.existsByEmail(email);
+    }
 }
