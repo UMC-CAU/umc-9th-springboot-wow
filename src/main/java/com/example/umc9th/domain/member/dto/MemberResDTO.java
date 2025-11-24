42b760d55d095bfe9f3f1630b161f5898a753489
Inyoung <piypiy9102bbb@gmail.com>
2025-11-24 10:23:12 +0900

    feat: 사용자 상세 정보 응답 dto

--- src/main/java/com/example/umc9th/domain/member/dto/MemberResDTO.java
+++ src/main/java/com/example/umc9th/domain/member/dto/MemberResDTO.java
@@ -2,6 +2,7 @@ package com.example.umc9th.domain.member.dto;
 
 import lombok.Builder;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class MemberResDTO {
 
@@ -10,4 +11,27 @@ public class MemberResDTO {
             Long memberId,
             LocalDateTime createAt
     ){}
+
+    //사용자 상세정보 응답 DTO
+    public record MemberDetailDTO(
+            // 기본 정보
+            Long memberId,
+            String name,
+            String nickname,
+            String email,
+            Integer gender, // 또는 String gender
+            Integer age,
+            String address,
+
+            // 선호 음식 목록 (이름만 반환)
+            List<String> preferFoodNames,
+
+            // 기타 정보
+            LocalDateTime createdAt
+            // LocalDateTime updatedAt
+    ) {}
+
+    public record PreferFoodListDTO(
+            List<String> preferFoodNames
+    ) {}
 }
