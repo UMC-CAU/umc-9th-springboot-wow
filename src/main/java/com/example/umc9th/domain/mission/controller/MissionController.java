fa53d5d8e893e44ef746dea8db6caa59795c30ee
Inyoung <piypiy9102bbb@gmail.com>
2025-11-25 09:13:02 +0900

    feat: 미션 도전 controller

---
+++ src/main/java/com/example/umc9th/domain/mission/controller/MissionController.java
@@ -0,0 +1,34 @@
+package com.example.umc9th.domain.mission.controller;
+
+import com.example.umc9th.domain.mission.dto.MissionResponseDTO;
+import com.example.umc9th.domain.member.entity.mapping.MemberMission;
+import com.example.umc9th.domain.mission.service.MemberMissionCommandService;
+import com.example.umc9th.domain.mission.exception.code.MissionSuccessCode;
+import com.example.umc9th.global.apiPayload.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class MissionController {
+
+    private final MemberMissionCommandService memberMissionCommandService;
+
+    @PostMapping("/missions/{missionId}/challenge")
+    public ApiResponse<MissionResponseDTO> challengeMission(
+            @PathVariable(name = "missionId") Long missionId) {
+
+        final Long HARDCODED_MEMBER_ID = 1000L;
+
+        MemberMission newChallenge = memberMissionCommandService.challengeMission(
+                HARDCODED_MEMBER_ID,
+                missionId
+        );
+
+        MissionResponseDTO responseDTO = MissionResponseDTO.toDTO(newChallenge);
+        
+        return ApiResponse.of(MissionSuccessCode.MISSION_CHALLENGE_SUCCESS, responseDTO);
+    }
+}
