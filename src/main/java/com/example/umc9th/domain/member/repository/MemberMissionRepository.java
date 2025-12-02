35ab12172e8735c21e07bbd54e8ec86658395970
Inyoung <piypiy9102bbb@gmail.com>
2025-12-02 22:22:07 +0900

    feat: page로 미션 조회

--- src/main/java/com/example/umc9th/domain/member/repository/MemberMissionRepository.java
+++ src/main/java/com/example/umc9th/domain/member/repository/MemberMissionRepository.java
@@ -4,24 +4,17 @@ import com.example.umc9th.domain.member.entity.Member;
 import com.example.umc9th.domain.member.entity.mapping.MemberMission;
 import com.example.umc9th.domain.mission.entity.Mission;
 import com.example.umc9th.domain.mission.enums.Status;
-import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-
-import java.util.List;
 
 public interface MemberMissionRepository extends JpaRepository<MemberMission, Long> {
 
     boolean existsByMemberAndMission(Member member, Mission mission);
 
-
-    @Query("SELECT mm FROM MemberMission mm JOIN mm.mission m WHERE mm.member.id = :memberId AND m.status IN :statusList")
-    Slice<MemberMission> findAllByMemberIdAndStatusIn(
-            @Param("memberId") Long memberId,
-            @Param("statusList") List<Status> statusList,
+    Page<MemberMission> findAllByMemberIdAndStatus(
+            Long memberId,
+            Status status,
             Pageable pageable
     );
 }
