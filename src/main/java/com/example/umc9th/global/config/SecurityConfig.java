fda1c6cd78e75f483e0858b4588b27d6975026e5
Inyoung <piypiy9102bbb@gmail.com>
2025-12-09 20:43:29 +0900

    feat:DaoAuthenticationProvider를 사용한 DB 인증 설정

--- src/main/java/com/example/umc9th/global/config/SecurityConfig.java
+++ src/main/java/com/example/umc9th/global/config/SecurityConfig.java
@@ -2,8 +2,13 @@ package com.example.umc9th.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity  //Spring Security 설정 활성화(직접 작성한 보안 설정이 Spring Security 기본 설정보다 우선 적용됨)
@@ -11,10 +16,11 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final String[] allowUris = {
-//							// Swagger 허용
-//            "/swagger-ui/**",
-//            "/swagger-resources/**",
-//            "/v3/api-docs/**",
+        // Swagger
+        "/sign-up",
+        "/swagger-ui/**",
+        "/swagger-resources/**",
+        "/v3/api-docs/**",
     };
 
     @Bean
@@ -29,6 +35,7 @@ public class SecurityConfig {
                         .defaultSuccessUrl("/swagger-ui/index.html", true)
                         .permitAll()
                 )
+                .csrf(AbstractHttpConfigurer::disable)
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login?logout")
@@ -37,4 +44,31 @@ public class SecurityConfig {
 
         return http.build();
     }
+   @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(
+            // Spring이 CustomUserDetailsService 구현체를 자동으로 주입합니다.
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        // CustomUserDetailsService를 사용하여 DB에서 사용자 정보를 로드하도록 설정
+        provider.setUserDetailsService(userDetailsService);
+
+        // PasswordEncoder를 사용하여 비밀번호를 비교하도록 설정
+        provider.setPasswordEncoder(passwordEncoder);
+
+        return provider;
+    }
+    /*
+    @Bean
+    public JwtAuthFilter jwtAuthFilter(){
+        return new JwtAuthFilter(jwtUtil, customUserDetailsService);
+    }*/
+
 }
